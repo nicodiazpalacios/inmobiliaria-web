7e5cc52f362eb639ea40d9d022663e9382c55a36
Nicolás Díaz <130724068+nicodiazpalacios@users.noreply.github.com>
2025-10-07 15:30:13 -0300

    Upload Inmobiliaria

---
+++ inmobiliaria/inmobiliaria/src/test/java/com/inmobiliaria/inmobiliaria/InmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmobiliaria.inmobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
